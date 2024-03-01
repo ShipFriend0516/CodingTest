1333c781ab57158097cdefbfce6303501598c610
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2024-03-02 04:34:37 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.96 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,7 +1,3 @@
 function solution(arr1, arr2) {
-
-    return arr1.reduce((acc, cur, rIdx) => {
-        const newRow = cur.reduce((acc,cur,cIdx)=> acc.concat(cur + arr2[rIdx][cIdx]),[])
-        return acc.concat([newRow])
-    },[])
+    return arr1.map((a,idx)=> a.map((aValue, idx2)=> aValue+= arr2[idx][idx2]))
 }
