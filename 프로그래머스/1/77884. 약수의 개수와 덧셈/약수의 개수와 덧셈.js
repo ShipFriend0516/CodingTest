836814e8dcfc2295e1067b740dcef1b40f35a2ae
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2024-03-02 02:31:12 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 3.65 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,13 @@
+function solution(left, right) {
+    let result = 0;
+    for(let i = left; i<= right; i++) {
+        let count = 0;
+        for(let j = 1; j<=i;j++) {
+            i%j === 0 ? count++ : '';
+        }
+        console.log(count)
+        count % 2 ? result -= i : result += i
+    }
+    
+    return result
+}
