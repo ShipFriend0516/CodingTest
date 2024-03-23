f8055930c972e73a1dff22206d135a93ef473674
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2024-03-24 03:19:38 +0900

    [level 2] Title: N개의 최소공배수, Time: 2.68 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -0,0 +1,20 @@
+function solution(arr) {
+    let condition = true;
+    let i=1;
+    let max = Math.max(...arr);
+    
+    while(condition) {
+        let number = max * i;
+        for(let j=0;j<arr.length;j++) {
+            if(number % arr[j] === 0) {
+                if(j===arr.length-1) {
+                    return number;
+                }
+                continue;
+            } else {
+                i++;
+                break;
+            }
+        }
+    }
+}
