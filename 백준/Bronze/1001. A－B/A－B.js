e54656e05c61c823688e3f132f97e846e8b273c1
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2024-06-30 14:56:53 +0900

    [Bronze V] Title: A-B, Time: 176 ms, Memory: 9480 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,16 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+})
+
+let input;
+rl.on('line', line=> {
+	input = line.split(' ').map(v=> parseInt(v))
+	rl.close()
+})
+
+rl.on('close', ()=> {
+	console.log(input[0] - input[1])
+})
