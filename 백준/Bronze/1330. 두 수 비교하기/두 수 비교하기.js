b687e9f6b509177578e66e1f4af904f49b3e10ae
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2024-07-06 12:16:23 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 112 ms, Memory: 9436 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
@@ -0,0 +1,26 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+let height = 0;
+let width = 0;
+let inputCount = 0;
+
+readline.on('line', function(line) {
+	input = line.split(' ')
+	readline.close();
+}).on('close', function(){ 
+	console.log(solution(input))
+    process.exit();
+});
+
+const solution = (input) => {
+	let [a,b] = input;
+	a = parseInt(a)
+	b = parseInt(b)
+	if(a>b) return '>'
+	else if(a<b) return '<'
+	else if(a===b) return '=='
+}
